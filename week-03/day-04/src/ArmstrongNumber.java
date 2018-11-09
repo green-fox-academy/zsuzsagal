da0778dfd232719c2aea861d3054e5bbc331f20e
Zsuzsa Gal <zsuzsa.k.gal@gmail.com>
2018-11-09 12:29:09 +0100

    adding ArmstrongNumber

---
+++ week-03/day-04/src/ArmstrongNumber.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        /*
+        What is Armstrong number?
+        An Armstrong number is an n-digit number that is equal to the sum of the nth powers of its digits.
+
+        Let's demonstrate this for a 4-digit number: 1634 is a 4-digit number, raise each digit to the fourth power,
+        and add: 1^4 + 6^4 + 3^4 + 4^4 = 1634, so it is an Armstrong number.
+        For a 3-digit number: 153 is a 3-digit number, raise each digit to the third power,
+        and add: 1^3 + 5^3 + 3^3 = 153, so it is an Armstrong number.
+        Exercise:
+        Write a simple program to check if a given number is an armstrong number.
+        The program should ask for a number.number E.g. if we type 371, the program should print out:
+        The 371 is an Armstrong number.
+        */
+
+        armstrongNumber(154);
+    }
+
+
+    public static void armstrongNumber(int number) {
+        String valueOfNumber = String.valueOf(number);
+        int length = valueOfNumber.length();
+        int sumOfDigits = 0;
+
+        for (int i = 0; i < length; i++) {
+            char a = valueOfNumber.charAt(i);
+            int digit = Character.getNumericValue(a);
+
+            sumOfDigits += Math.pow(digit, length);
+        }
+        if (sumOfDigits == number) {
+            System.out.println("The " + number + " is an Armstrong number.");
+        } else {
+            System.out.println("The " + number + " is NOT an Armstrong number.");
+        }
+    }
+}
+
