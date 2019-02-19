f4fbe59ae66eb51a660c7fe6514641b80184e6c5
Zsuzsa Gal <zsuzsa.k.gal@gmail.com>
2019-02-19 10:43:17 +0100

    refactor Sum and SumTest

--- week-05/day-04/src/sum/Sum.java
+++ week-05/day-04/src/sum/Sum.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Sum {
-//    Create a sum method in your class which has an ArrayList of Integers as parameter
-//    It should return the sum of the elements in the list
 
     public int sumOfArray(ArrayList<Integer> numbers) {
         int sumOfElements = 0;
@@ -19,9 +17,8 @@ public class Sum {
         return sumOfElements;
     }
 
-
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList());
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList());
         Sum sum1 = new Sum();
         sum1.sumOfArray(numbers);
     }
--- week-05/day-04/src/sum/SumTest.java
+++ week-05/day-04/src/sum/SumTest.java
@@ -2,24 +2,11 @@ package sum;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SumTest {
-//    Follow these steps:
-//    Add a new test case
-//    Instantiate your class
-//    create a list of integers
-//    use the assertEquals to test the result of the created sum method, Run them
-//    Create different tests where you test your method with an empty list
-//    with a list with one element in it
-//    with multiple elements in it
-//    with a null
-//    Run them
-//    Fix your code if needed
 
     Sum sum;
 
@@ -30,28 +17,29 @@ class SumTest {
 
     @Test
     void testSum() {
-        int expectedResult = 150;
-        ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(4, 28, 7, 10, 15, 86));
-        assertEquals(expectedResult, sum.sumOfArray(numbers));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(4, 28, 7, 10, 15, 86));
+        int result = sum.sumOfArray(numbers);
+        assertEquals(150, result);
     }
 
     @Test
     void testSumWithEmptyList() {
-        int expectedResult = 0;
-        ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList());
-        assertEquals(expectedResult, sum.sumOfArray(numbers));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList());
+        int result = sum.sumOfArray(numbers);
+        assertEquals(0,result );
     }
 
     @Test
     void testSumWithOneElement() {
-        ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(86));
-        int expectedResult = numbers.get(0);
-        assertEquals(expectedResult, sum.sumOfArray(numbers));
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(86));
+        int result = sum.sumOfArray(numbers);
+        assertEquals(86, result);
     }
 
     @Test
     void testSumWithNull() {
-        int expectedResult = 0;
-        assertEquals(expectedResult, sum.sumOfArray(null));
+        int result = sum.sumOfArray(null);
+        assertEquals(0, result);
     }
+
 }
